bc0f501605367b875734d74af8ef244845f45c11
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2026-03-06 15:33:04 +0100

    Add tags style in MarkdownPostLayout and rss package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://reliable-gnome-2a4623.netlify.app/",
+});
